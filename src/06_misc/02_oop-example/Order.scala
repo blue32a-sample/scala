0069f5ebb5fe57fb0a9b83e2a13e62cfba6fa702
blue32a <blue32a.jp@gmail.com>
2022-01-16 19:20:02 +0900

    An OOP Example

---
+++ src/06_misc/02_oop-example/Order.scala
@@ -0,0 +1,22 @@
+package pizza
+
+import scala.collection.mutable.ArrayBuffer
+
+class Order (
+    var pizzas: ArrayBuffer[Pizza],
+    var customer: Customer
+) {
+    def addPizza(p: Pizza): Unit = pizzas += p
+    def removePizza(p: Pizza): Unit = pizzas -= p
+
+    // need to implement these
+    def getBasePrice(): Int = ???
+    def getTaxes(): Int = ???
+    def getTotalPrice(): Int = ???
+
+    def printOrder(): Unit = {
+        for (p <- pizzas) {
+            println(p)
+        }
+    }
+}
